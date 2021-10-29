3f5868e3b5915f90958c1b91885c333a3c2d5809
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-10-29 20:31:45 -0300

    MackWeb - class7 Calculo de Médias

--- Mack-FundWeb7/CalculoDeMedias/calculoDeMedias.js
+++ Mack-FundWeb7/CalculoDeMedias/calculoDeMedias.js
@@ -1,9 +1,14 @@
-/* Digita a sua nota */
-let notasDasProvas = [10, 8.5, 3.0];
-notasDasProvas.push(5.5); /* adicionando a quarta nota na lista usando o método push() */
-console.log(notasDasProvas)
-
-let media = (notasDasProvas[0],notasDasProvas[1],notasDasProvas[2],notasDasProvas[3])/notasDasProvas.length;
-console.log(media);
-
-
+const notas = prompt('Digite as suas notas:');
+let listaNotas = ['primeira','segunda','terceira','quarta'];
+let notas = [0,0,0,0];
+for(var i = 0; i < 4; i++){
+    do {
+        let res = prompt("Entre com a " + listaNotas[i] + " nota:");
+        if (res != null && res != "") {
+            notas[i] = parseFloat(res);
+        }
+    }while(res == null || res == "");
+}
+let media = notas[0] + notas[1] + notas[2] + notas[3];
+media = media / 4;
+alert("notas = " + notas + "\nmédia = " + media);
