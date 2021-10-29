a2a5c1ac5d0d341c3ea6746d8b278be452105b7a
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-10-28 23:45:05 -0300

    MackWeb7 - Praticando JavaScript

---
+++ Mack-FundWeb7/CalculoDeMedias/calculoDeMedias.js
@@ -0,0 +1,9 @@
+/* Digita a sua nota */
+let notasDasProvas = [10, 8.5, 3.0];
+notasDasProvas.push(5.5); /* adicionando a quarta nota na lista usando o método push() */
+console.log(notasDasProvas)
+
+let media = (notasDasProvas[0],notasDasProvas[1],notasDasProvas[2],notasDasProvas[3])/notasDasProvas.length;
+console.log(media);
+
+
