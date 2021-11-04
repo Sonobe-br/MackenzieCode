352a8808500080b5142db34c26e985ae7a38bc56
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-11-04 00:15:47 -0300

    MackWeb Class8 - ao abrir o arquivo HTML no navegador o javascript faz uma saudação de boas vindas e apresenta numa espécie de popup alguns dados do objeto cadastrado. Finalizo com um imagem fotografica da unidade Mackenzie Higienópolis decorando o fundo da página, mas a cereja do bolo é a apresentação de informações no estilo calendário baseado no fuso horário local.

---
+++ Mack-FundWeb8/minhaCredencial.js
@@ -0,0 +1,22 @@
+class Credencial{
+    nome;
+    sobrenome;
+    idade;
+    sexo;
+    cidade;
+    curso;
+    universidade;    
+}
+
+const credencial1 = new Credencial();
+credencial1.nome = 'Daniel';
+credencial1.sobrenome = 'Sonobe Silveira';
+credencial1.idade = 43;
+credencial1.sexo = 'Masculino';
+credencial1.cidade = 'São Paulo/SP';
+credencial1.curso = 'Análise e Desenvolvimento de Sistemas';
+credencial1.universidade = 'Universidade Presbiteriana Mackenzie';
+
+alert(`Olá meu nome é ${credencial1.nome} ${credencial1.sobrenome}, tenho ${credencial1.idade} sexo ${credencial1.sexo}, morador da cidade de ${credencial1.cidade}, estou cursando ${credencial1.curso} na ${credencial1.universidade}.`);
+console.log(credencial1); //concatei as variáveis no método template strings.
+
